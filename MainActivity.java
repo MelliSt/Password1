da2e5c4cfb8776d502df3967390996e4b7c8a991
Melanie Strauss <52stme1bwi@hft-stuttgart.de>
2018-04-04 09:24:23 +0200

    Test

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.user.password2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.goodiebag.pinview.Pinview;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Pinview pinview = (Pinview)findViewById(R.id.pinView);
+        pinview.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @Override
+            public void onDataEntered(Pinview pinview, boolean b) {
+                Toast.makeText(MainActivity.this, ""+pinview.getValue(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
